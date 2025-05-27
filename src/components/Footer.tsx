36682d0b2f011b5780676cd5c5f78bdd0b992910
Freddyx14 <freddynanez8@gmail.com>
2025-05-27 03:18:39 -0500

    src/components/Footer.tsx

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -10,7 +10,7 @@ const Footer = () => {
             <Link to="/" className="flex items-center">
               <span className="text-2xl font-bold text-chambier-bright">Chambier</span>
               <img 
-                src="https://imgur.com/a/aw6l9pd" 
+                src="https://i.imgur.com/mrY1Jqa.png" 
                 alt="Trabajador Chambier" 
                 className="h-7 ml-1" 
               />
